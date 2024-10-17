138512ed003c08ff5d2dc1f6afe25ee84825b7c8
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2024-10-17 11:15:57 +0200

    chore(controller): update user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  NotFoundException,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
@@ -13,6 +21,13 @@ export class UsersController {
   @ApiOkResponse({ type: [UserEntity] })
   async getAllUsers(): Promise<User[]> {
     return this.usersService.findMany();
+  }
+
+  @Get('/me')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: UserEntity })
+  async getCurrentUser(@CurrentUser() user: User): Promise<User> {
+    return this.usersService.findOneById(user.id);
   }
 
   @Get('/:id')
